c5e9ecacdb169a08bd9e3cf9c0ecf2549902745c
Bernard-code <bernard.kacprzak93@gmail.com>
2026-08-29 17:55:23 +0200

    change question card layout

--- src/app/main/components/question-card/question-card.component.ts
+++ src/app/main/components/question-card/question-card.component.ts
@@ -2,7 +2,6 @@ import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { Question } from '../../model/question.model';
 import { combineLatest, tap } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { StateService } from '../../services/state.service';
 import { isNil } from '../../utils/is-nil.util';
@@ -13,7 +12,6 @@ import { isNil } from '../../utils/is-nil.util';
   styleUrl: './question-card.component.scss',
   standalone: true,
   imports: [
-    MatButtonModule,
     MatIconModule,
   ],
 })
@@ -23,19 +21,29 @@ export class QuestionCardComponent implements OnInit {
 
   public questionId: number;
   public question: Question;
+  public questionIndex = 0;
+  public questionCount = 0;
   public showAnswer = false;
 
   public ngOnInit(): void {
     combineLatest([
       this.stateService.currentQuestionId$,
+      this.stateService.currentCategoryId$,
       this.stateService.questions$,
     ]).pipe(
-      tap(([questionId, questions]: [number, Question[]]) => {
+      tap(([questionId, categoryId, questions]: [number, number, Question[]]) => {
         if (questionId !== this.questionId) {
           this.showAnswer = false;
         }
         this.questionId = questionId;
         this.question = questions.find((question: Question) => question.id === questionId);
+
+        const inCategory = questions
+          .filter((question: Question) => Number(question.category) === Number(categoryId))
+          .sort((a, b) => a.position - b.position);
+        this.questionCount = inCategory.length;
+        const index = inCategory.findIndex((question: Question) => question.id === questionId);
+        this.questionIndex = index === -1 ? 0 : index + 1;
       }),
       takeUntilDestroyed(this.destroyRef),
     ).subscribe();
